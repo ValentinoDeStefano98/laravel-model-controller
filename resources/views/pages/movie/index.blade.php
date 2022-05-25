8022daafb3dac77991f3488a4a2e712329444a49
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-25 17:14:36 +0200

    Some style added

--- resources/views/pages/movie/index.blade.php
+++ resources/views/pages/movie/index.blade.php
@@ -5,12 +5,12 @@
 @section('content')
     <div class="row">
         @forelse ($movies as $key => $movie)
-            <div class="col-4 card">
+            <div class="col-4 card text-center">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text">{{ $movie->nationality }}</p>
-                    <p class="card-text">{{ $movie->date }}</p>
-                    <p class="card-text">{{ $movie->vote }}</p>
+                    <h3 class="card-title">{{ $movie->title }}</h3>
+                    <p class="card-text"><span class="text-danger">Language:</span>  {{ $movie->nationality }}</p>
+                    <p class="card-text"><span class="text-danger">Release:</span> {{ $movie->date }}</p>
+                    <p class="card-text"><span class="text-danger">Score:</span> {{ $movie->vote }}</p>
                 </div>
 
             </div>
