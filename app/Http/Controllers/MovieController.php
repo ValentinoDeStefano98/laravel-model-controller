b4781de05fae2090526bbb359ae8e0d251e931f2
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-25 17:04:14 +0200

    model fixed

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,7 +10,7 @@ class MovieController extends Controller
 {
     public function index(){
         $movies = Movie::all();
-
+        dd($movies);
         return view('pages.movie.index', compact('movies'));
     }
 }
