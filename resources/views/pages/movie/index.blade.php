aa24ce3d8c7fa2db4f0ac87f6156b961be55211a
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-25 17:45:06 +0200

    single page added

--- resources/views/pages/movie/index.blade.php
+++ resources/views/pages/movie/index.blade.php
@@ -9,8 +9,8 @@
                 <div class="card-body">
                     <h3 class="card-title">{{ $movie->title }}</h3>
                     <p class="card-text"><span class="text-danger">Language:</span>  {{ $movie->nationality }}</p>
-                    <p class="card-text"><span class="text-danger">Release:</span> {{ $movie->date }}</p>
-                    <p class="card-text"><span class="text-danger">Score:</span> {{ $movie->vote }}</p>
+                    {{-- <p class="card-text"><span class="text-danger">Release:</span> {{ $movie->date }}</p>
+                    <p class="card-text"><span class="text-danger">Score:</span> {{ $movie->vote }}</p> --}}
                     <a href="{{route('movie', ['id' => $movie->id])}}" class="btn btn-primary">Info</a>
                 </div>
             </div>
